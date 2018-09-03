b3c4d29b7063c984bd70867818e8e590d6bb44df
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-03 01:59:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+let neighborhoodId = 0;
 
 class Neighborhood {
   constructor(name) {
+    this.id = ++neighborhoodId;
     this.name = name;
   }
 }
