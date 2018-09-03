3c911e6de5de64e18b262504d126940596577baf
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-03 02:29:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,7 +50,9 @@ class Customer {
   }
 
   totalSpent() {
-    return this.meals().reduce((total, meal) => (total += meal.price), 0);
+    return this.meals().reduce((total, meal) => {
+
+    }(total += meal.price), 0);
   }
 }
 
